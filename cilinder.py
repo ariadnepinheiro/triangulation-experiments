00788fbe40432ba10ff666339b5a691c2fd1b3a7
Ariadne Pinheiro <ariadnepinheiro@id.uff.br>
2021-10-30 02:48:00 -0300

    Update cilinder.py

--- cilinder.py
+++ cilinder.py
@@ -1,5 +1,8 @@
-// Utilizando a representação paramétrica, escreva um algoritmo para gerar uma triangulação para um cilindro de altura 2,
-// cuja base é um círculo de raio 3 posicionado na origem do plano XY
+"""
+Utilizando a representação paramétrica, escreva um algoritmo para 
+gerar uma triangulação para um cilindro de altura 2, cuja base é u
+m círculo de raio 3 posicionado na origem do plano XY
+"""
 
 from OpenGL.GL import *
 
